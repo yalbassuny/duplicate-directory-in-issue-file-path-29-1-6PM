081e262d7fb861615a3b2de23132b97005c8ca14
Yasmeen Farag <yasmeen.farag@aurea.com>
2020-01-29 18:46:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# duplicate-directory-in-issue-file-path
---
+++ src/pathA/pathB/pathA/pathC/pathB/Main.java
@@ -0,0 +1,15 @@
+package pathA.pathB.pathA.pathC.pathB;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        switch(args[0]) {
+            case "0":
+                System.out.println('0');
+            case "1":
+                System.out.println('1');
+        }
+    }
+}
